5b067360bed0c80726d6d4fb8e7e5376e41532e8
gavand-sagar <gavand.sagar.s@gmail.com>
2022-06-26 19:59:21 +0530

    es 6

---
+++ main.js
@@ -0,0 +1,8 @@
+import { user } from './util.js'
+import { showGlobalAlert } from './util.js'
+
+let { email, name, age } = user
+
+console.log(name)
+
+showGlobalAlert()
---
+++ util.js
@@ -0,0 +1,13 @@
+
+export let user = {
+    name: 'Nitin',
+    age: 80,
+    contact: '21312312',
+    email: 'asdf@adsf.ccc',
+    address: "Mumbai"
+}
+
+
+export function showGlobalAlert(){
+    alert('Something went wrong')
+}
